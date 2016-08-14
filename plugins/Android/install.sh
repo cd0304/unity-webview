24f07f94966779f78380a48e0ff1f261926bf17c
cd0304 <12487947@qq.com>
2016-08-15 00:27:19 +0800

    Update install.sh

--- plugins/Android/install.sh
+++ plugins/Android/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-UNITYLIBS=`find -L /Applications/Unity | grep classes.jar | tail -1`
+UNITYLIBS=find -L D:/softwave/Unity | grep classes.jar | tail -1
 DSTDIR="../../build/Packager/Assets/Plugins/Android"
 export ANT_OPTS=-Dfile.encoding=UTF8
-android update project -t android-18 -p .
+android.bat update project -t android-18 -p .
 mkdir -p libs
 cp $UNITYLIBS libs
 ant "-Djava.compilerargs=-Xlint:deprecation" release
